aa05dc2fda301eba00ca394e857f9b9155c94ec7
Apple <apple@Apples-MacBook-Pro-115.local>
2019-08-06 10:04:05 -0400

    initial commit

---
+++ KWK Day 1.playground/Contents.swift
@@ -0,0 +1,71 @@
+//import UIKit
+//
+//let greetingSayHello = "Hi!"
+////print(greetingSayHello)
+////greetingSayHello  = "hola"
+//
+////print(greetingSayHello)
+//////print ("Welcome to Kode with Klossy")
+//////print("\(greetingSayHello) shelia!")
+////var first = "Karlie"
+////var last = "Kloss"
+////print ("\(first)\(last)")
+////print ("\(last)\(first)")
+//var interger = 4
+//var double = 5.0
+////print (interger * double)
+//print(Int(double) * interger)
+//var a = 12
+//var b = 65
+//var c = 31
+//var d = 98
+////var x = ((a)+(b)+(c)+(d))
+////print ((x/4))
+//print (((a)+(b)+(c)+(d))/4)
+//var name = "Sumaia"
+//var address = "Dearborn"
+//var birthday = "August 30th"
+//var gradeLevel = "12th grade"
+//var age = "16"
+//var eyeColor = "Brown"
+//var favoriteColor = "pastel pink or black if im depressed"
+//var wage = 10.25
+//var hour = 20
+//
+//print ((wage) * Double(hour))
+//print (5 < 3)
+//12 < 7
+//6 != 8
+//7 == 7
+//var luckynum = 7
+//luckynum < 10
+//var dogage = 4
+//if dogage < 2 {print ("You are a puppy 🐶!")}
+//else if dogage > 12 {print ("You are elderly!")}
+//else {print("Thats ruff!")}
+//var favoritefood = "Ice cream"
+//if favoritefood == "Chipotle" {print ("Really? I love their burritos! Yum!!!!")}
+//else if favoritefood == "Starbucks"{print ("ew stop being basic") }
+//else if favoritefood == "Ice cream" {print ("Brrrrrr!")}
+//else {print ("ooooo exotic!")}
+//var hasAccount = true
+//if hasAccount == false {print ("Please try again!")}
+//else {print ("Account does not exist. Sign up!")}
+//func walkdog()
+//{
+//print("Put shoes on")
+//print ("Attach leash to dog")
+//print ("Go outside")
+//print("Walk along the side walk")
+//}
+//walkdog()
+//func pbj()
+//{
+//print
+//("Take out bread")
+//print
+func walkdog(numberofdogs : Int ) -> Int {
+//    print ("there are \(numberofdogs) dogs in the house")
+    return numberofdogs
+}
+//print (walkdog(numberofdogs: 4))

2ad1b25ea6ff48e6bb0e8c524d2568f2924e9b0a
Apple <apple@Apples-MacBook-Pro-115.local>
2019-08-06 13:22:57 -0400

    collections notes added

---
+++ Collections.playground/Contents.swift
@@ -0,0 +1,41 @@
+//////creating empty arrays
+////var arrayofstrings = [String]()
+////var arrayofintergers = [Int]()
+//////creating an array with data
+//var friendsofkarlie = ["Sumaia","T-swift","josh","Selena"]
+////// access the array
+////// "Sumaia" is at index position 0
+////// to print "josh"
+////print (friendsofkarlie[2])
+//////what if my order of info or info is wrong?
+//friendsofkarlie[2] = "elmo"
+//friendsofkarlie.append("Zena")
+//friendsofkarlie.remove (at : 1)
+////print(friendsofkarlie)
+////var rolemodels = ["Zena","Rana","Karlie"]
+////rolemodels[1]
+////rolemodels.append("Myself")
+////rolemodels[2] = "Selena"
+////rolemodels.remove (at : 2)
+////print (rolemodels)
+////iterating over arrays must have an array with data
+//// use the keywords for and in
+//for friend in friendsofkarlie{
+//    print ("Hello \(friend)" )
+//}
+//var perfectTen : [String : String] = [:]
+//print (perfectTen)
+//var perfectEleven = ["Almond flour" : "3 and 1/2 cups", "Gluten free oats" : "2 cups", "dark chocolate chips" : "1 cup"]
+//// almond flour gluten free oats and dark chocolate chips are keys
+//// 3 and 1/2 cups, 2 cups, 1 cups are values
+//perfectEleven["dark chocolate chips"] = "2 cups"
+//print (perfectEleven.keys)
+//print(perfectEleven.values)
+//print(perfectEleven["Gluten free oats"]!)
+//perfectEleven["vanilla"] = "1 tsp"
+//print (perfectEleven)
+//perfectEleven["Gluten free oats"] = nil
+//print (perfectEleven)
+var peopleinmylife = ["Zena" : "Kinda friend", "Sam" : "Brother", "Rana" : "sarcastic friend"]
+print (peopleinmylife)
+print (peopleinmylife["Zena"]!)
